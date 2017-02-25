754c517e674773c08fc858d9bace89a74f2681b1
Jam Risser <jamrizzi@users.noreply.github.com>
2017-02-25 03:43:34 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-_USER=$USER
-_FULL_NAME="First Last"
-_PROJECT=$(echo "${PWD##*/}")
+DM_USER=$USER
+DM_FULL_NAME="First Last"
+DM_PROJECT=$(echo "${PWD##*/}")
 
 # gather information
-read -p "User ($_USER): " _USER_NEW
-if [ $_USER_NEW ]; then
-    _USER=$_USER_NEW
+read -p "User ($DM_USER): " DM_USER_NEW
+if [ $DM_USER_NEW ]; then
+    DM_USER=$DM_USER_NEW
 fi
-read -p "Full Name ($_FULL_NAME): " _FULL_NAME
-if [ $_FULL_NAME_NEW ]; then
-    _FULL_NAME=$_FULL_NAME_NEW
+read -p "Full Name ($DM_FULL_NAME): " DM_FULL_NAME
+if [ $DM_FULL_NAME_NEW ]; then
+    DM_FULL_NAME=$DM_FULL_NAME_NEW
 fi
-read -p "Project ($_PROJECT): " _PROJECT_NEW
-if [ $_PROJECT_NEW ]; then
-    _PROJECT=$_PROJECT_NEW
+read -p "Project ($DM_PROJECT): " DM_PROJECT_NEW
+if [ $DM_PROJECT_NEW ]; then
+    DM_PROJECT=$DM_PROJECT_NEW
 fi
 
-curl -L https://raw.githubusercontent.com/jamrizzi/dockmake/master/Dockerfile | tee Dockerfile
-curl -L https://raw.githubusercontent.com/jamrizzi/dockmake/master/Makefile | tee Makefile
+curl -oL https://raw.githubusercontent.com/jamrizzi/dockmake/master/Dockerfile
+curl -oL https://raw.githubusercontent.com/jamrizzi/dockmake/master/Makefile
 
-sed -i -e s/\${user\}/$_USER/g Makefile
-sed -i -e s/\${user\}/$_USER/g Dockerfile
+sed -i -e s/\${user\}/$DM_USER/g Makefile
+sed -i -e s/\${user\}/$DM_USER/g Dockerfile
 
-sed -i -e s/\${full_name\}/$_FULL_NAME/g Makefile
-sed -i -e s/\${full_name\}/$_FULL_NAME/g Dockerfile
+sed -i -e s/\${full_name\}/$DM_FULL_NAME/g Makefile
+sed -i -e s/\${full_name\}/$DM_FULL_NAME/g Dockerfile
 
-sed -i -e s/\${project\}/$_PROJECT/g Makefile
-sed -i -e s/\${project\}/$_PROJECT/g Dockerfile
+sed -i -e s/\${project\}/$DM_PROJECT/g Makefile
+sed -i -e s/\${project\}/$DM_PROJECT/g Dockerfile
